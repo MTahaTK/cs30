0ef32b7591e128d7081aa4656c71584bbc84c907
MTahaTK <50962623+MTahaTK@users.noreply.github.com>
2019-05-29 11:58:04 -0600

    updated debugger.py
    now includes (relatively) completed bp_ins() function and preliminary debug_file_exec() function.

--- Python Debugger/debugger.py
+++ Python Debugger/debugger.py
@@ -1,8 +1,24 @@
-def bp_ins(filename):
+def bp_ins(filename, line):
     """Inserts breakpoints into code"""
-    with open(filename, 'r+') as f:
+    with open(filename, 'r') as f:
         lines = f.readlines()
-        for line in lines:
-            print(line)
+    lines.insert(line-1, "")
+    lines[line-1] = 'breakpoint()\n'
+    with open(f"break_{filename}", 'w+') as f:
+        f.writelines(lines)
 
-bp_ins("text.txt")
+def debug_file_exec(filename):
+    """Executes provided file through the console"""
+    exec(open(f"break_{filename}").read())
+
+filename = 'text.py'
+bp_ins(filename, 2)
+debug_file_exec(filename)
+
+
+
+# print('ok')
+# print('no')
+# print('9')
+# print(17)
+# print('never')
