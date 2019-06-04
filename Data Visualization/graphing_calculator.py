313ec940bc31ad4f823773f3cdc3a327f41bc1af
MTahaTK <50962623+MTahaTK@users.noreply.github.com>
2019-06-04 12:03:01 -0600

    fixed matplotlib issue, Equation now in use

--- Data Visualization/graphing_calculator.py
+++ Data Visualization/graphing_calculator.py
@@ -1,7 +1,13 @@
-import matplotlib.pyplot as pplt
+import matplotlib
 from calculator_base import *
+from Equation import Expression
 
 # calculator()
 
+msg = "Enter an algebraic expression in mathematical syntax: "
+msg += "\nNOTE: MULTIPLICATION MUST USE THE * OPERATOR: "
+fn = input(msg)
 
-fn = input("Enter an algebraic expression in mathematical syntax: ")
+fn = Expression(fn)
+print(fn)
+print(fn(3))
